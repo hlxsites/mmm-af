800720ef9e84c387e4b0e7cdce126b769da1068e
David Catalan <catalan@adobe.com>
2022-11-29 12:01:40 +0100

    aria - fix links do not have a discernible name in footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,10 +1,25 @@
 import { readBlockConfig } from '../../scripts/scripts.js';
 
 /**
- * loads and decorates the footer
+ * Handles footer links
+ * - sets a default aria-label attribute when it has no discernible name
+ * @param {Element} footer The footer element
+ */
+ function handleLinks(footer) {
+  footer.querySelectorAll('a').forEach((a) => {
+    const href = a.getAttribute('href');
+    if (href && a.textContent.trim() === '' && !a.hasAttribute('aria-label')) {
+      const u = new URL(href);
+      a.setAttribute('aria-label', u.hostname);
+    }
+  });
+}
+
+
+/**
+ * Loads and decorates the footer
  * @param {Element} block The header block element
  */
-
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
   block.textContent = '';
@@ -14,6 +29,7 @@ export default async function decorate(block) {
   const html = await resp.text();
   const footer = document.createElement('div');
   footer.innerHTML = html;
+  handleLinks(footer);
   // await decorateIcons(footer);
   block.append(footer);
 }
